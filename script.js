9b10078927209af2ae10d77eb8d484a31aa2193b
Forkentiney <jseverud@proton.me>
2026-01-14 15:38:40 -0600

    Fixed style issues and added keyboard function

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-const buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll(".button");
 const display = document.getElementById("display");
 
 const one = document.getElementById("one").firstElementChild;
 const two = document.getElementById("two").firstElementChild;
 const three = document.getElementById("three").firstElementChild;
+const four = document.getElementById("four").firstElementChild;
+const five = document.getElementById("five").firstElementChild;
+const six = document.getElementById("six").firstElementChild;
+const seven = document.getElementById("seven").firstElementChild;
+const eight = document.getElementById("eight").firstElementChild;
+const nine = document.getElementById("nine").firstElementChild;
+const zero = document.getElementById("zero").firstElementChild;
+const decimal = document.getElementById("decimal").firstElementChild;
+const power = document.getElementById("power").firstElementChild;
+const clear = document.getElementById("clear").firstElementChild;
+const backspace = document.getElementById("back").firstElementChild;
+const divide = document.getElementById("divide").firstElementChild;
+const multiply = document.getElementById("multiply").firstElementChild;
+const subtract = document.getElementById("subtract").firstElementChild;
+const add = document.getElementById("add").firstElementChild;
+const equals = document.getElementById("equals").firstElementChild;
 
 let onScreen = [];
 
@@ -11,6 +27,7 @@ addEventListener("keydown", useKey);
 addEventListener("keyup", stopUseKey);
 
 function useKey(key) {
+  console.log(key.code);
   if (key.code == "Digit1") {
     one.classList.add("pressed");
     useCalc("one");
@@ -20,6 +37,34 @@ function useKey(key) {
   } else if (key.code == "Digit3") {
     three.classList.add("pressed");
     useCalc("three");
+  } else if (key.code == "Digit4") {
+    four.classList.add("pressed");
+    useCalc("four");
+  } else if (key.code == "Digit5") {
+    five.classList.add("pressed");
+    useCalc("five");
+  } else if (key.code == "Digit6") {
+    six.classList.add("pressed");
+    useCalc("six");
+  } else if (key.code == "Digit7") {
+    seven.classList.add("pressed");
+    useCalc("seven");
+  } else if (key.code == "Digit8") {
+    eight.classList.add("pressed");
+    useCalc("eight");
+  } else if (key.code == "Digit9") {
+    nine.classList.add("pressed");
+    useCalc("nine");
+  } else if (key.code == "Digit0") {
+    zero.classList.add("pressed");
+    useCalc("zero");
+  } else if (key.code == "Backspace") {
+    backspace.classList.add("pressed");
+    useCalc("back");
+  } else if (key.code == "Enter") {
+    equals.classList.add("pressed");
+    equals.focus();
+    useCalc("equals");
   };
 };
 
@@ -30,6 +75,24 @@ function stopUseKey(key) {
     two.classList.remove("pressed");
   } else if (key.code == "Digit3") {
     three.classList.remove("pressed");
+  } else if (key.code == "Digit4") {
+    four.classList.remove("pressed");
+  } else if (key.code == "Digit5") {
+    five.classList.remove("pressed");
+  } else if (key.code == "Digit6") {
+    six.classList.remove("pressed");
+  } else if (key.code == "Digit7") {
+    seven.classList.remove("pressed");
+  } else if (key.code == "Digit8") {
+    eight.classList.remove("pressed");
+  } else if (key.code == "Digit9") {
+    nine.classList.remove("pressed");
+  } else if (key.code == "Digit0") {
+    zero.classList.remove("pressed");
+  } else if (key.code == "Backspace") {
+    backspace.classList.remove("pressed");
+  } else if (key.code == "Enter") {
+    equals.classList.remove("pressed");
   };
 };
 
@@ -41,6 +104,9 @@ function useCalc(id) {
   if (id == "clear") {
     onScreen = [];
     display.textContent = onScreen;
+  } else if (id == "back") {
+    onScreen.pop();
+    display.textContent = onScreen.join("");
   } else if (onScreen.length > 9) {
     display.textContent = "ERR";
   } else if (id == "one") {
@@ -51,6 +117,27 @@ function useCalc(id) {
     display.textContent = onScreen.join("");
   } else if (id == "three") {
     onScreen.push(3);
+    display.textContent = onScreen.join("");
+  } else if (id == "four") {
+    onScreen.push(4);
+    display.textContent = onScreen.join("");
+  } else if (id == "five") {
+    onScreen.push(5);
+    display.textContent = onScreen.join("");
+  } else if (id == "six") {
+    onScreen.push(6);
+    display.textContent = onScreen.join("");
+  } else if (id == "seven") {
+    onScreen.push(7);
+    display.textContent = onScreen.join("");
+  } else if (id == "eight") {
+    onScreen.push(8);
+    display.textContent = onScreen.join("");
+  } else if (id == "nine") {
+    onScreen.push(9);
+    display.textContent = onScreen.join("");
+  } else if (id == "zero") {
+    onScreen.push(0);
     display.textContent = onScreen.join("");
   };
 };
